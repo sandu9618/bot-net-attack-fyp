fbe8bb08d018ceef5ee0f8f37136c73e9efba18e
sandu9618 <sandunipavithra9611@gmail.com>
2021-01-03 12:33:13 +0530

    temp-corrected

--- MiddleMan/GN1/execute-on-gateway.sh
+++ MiddleMan/GN1/execute-on-gateway.sh
@@ -1,7 +1,12 @@
 Yellow=$'\e[1;33m'
 Reset=$'\e[0;m'
 
-sshpass -p ijkl ssh -o "StrictHostKeyChecking no" -t -t root@10.0.0.112 -p 22  & #TODO IP should be changed according to gateway
+createPath() {
+  mkdir /root/results
+}
+
+sshpass -p ijkl ssh -o "StrictHostKeyChecking no" -t -t root@10.0.0.112 -p 22 & #TODO IP should be changed according to gateway
 sleep 1
+sshpass -p ijkl ssh -o "StrictHostKeyChecking no" root@10.0.0.112 "$(declare -f createPath);createPath "
 echo "$Yellow Execute on gateway 10.0.0.112 $Reset"
 echo ijkl | sudo nc -l -p 12345 > /root/results/gateway-n1.pcap
--- MiddleMan/run-execute-on-gateway.sh
+++ MiddleMan/run-execute-on-gateway.sh
@@ -1,2 +1,3 @@
-chmod +x MiddleMan/GN1/execute-on-gateway.sh
-sh MiddleMan/GN1/execute-on-gateway.sh
+p=$(pwd)
+chmod +x "$p"/GN1/execute-on-gateway.sh
+sh "$p"/execute-on-gateway.sh
